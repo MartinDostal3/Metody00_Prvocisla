12f7521fa100ff2c03502826770cec2a302a7102
dostalm.04 <dostalm.04@spst.eu>
2023-04-17 08:33:12 +0200

    Přidejte soubory projektu.

---
+++ Metody00_Prvocisla/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace String01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(28, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(85, 29);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "13";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(135, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(454, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ověří, zda zadané číslo je prvočíslo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(135, 76);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(454, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "1. Optimalizace";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(135, 118);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(454, 29);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "2. Optimalizace";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button4.Location = new System.Drawing.Point(135, 160);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(454, 29);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "3. Optimalizace";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button5.Location = new System.Drawing.Point(135, 203);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(454, 29);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Můžeme zkoušet jen liché dělitele";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button6.Location = new System.Drawing.Point(135, 245);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(454, 29);
+            this.button6.TabIndex = 6;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(138, 308);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(259, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Vysvětli Eratosthenovo síto";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 393);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Metody00_Prvocisla/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace String01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private bool JePrvocislo00(int n)   //Z definice
+        {
+            //Spočteme všechny dělitele čísla a pokud má přesně dva dělitele, jedná se o prvočíslo
+            int pocetDelitelu = 0;
+            for (int delitel = 1; delitel <= n; delitel++)
+            {
+                if (n % delitel == 0) pocetDelitelu++;
+            }
+            return pocetDelitelu == 2;
+            
+        }
+
+        private bool JePrvocislo01(int n)
+        {
+            //1. optimalizace
+            //Zredukujeme počet zkoušených dělitelů
+            //Budeme zkoušet dělitele od 2 do poloviny n, pokud nenajdeme dělitele jedná se o prvočíslo
+            //Zdůvodnění:
+            //Žádné číslo nemá většího VLASTNÍHO dělitele než je jeho polovina
+
+            
+            bool prvocislo = true;
+            if (n == 1) prvocislo = false;
+            for (int delitel = 2; delitel <= n / 2; ++delitel)
+            {
+                if (n % delitel == 0) prvocislo = false;
+            }
+
+            return prvocislo;
+            
+        }
+
+        private bool JePrvocislo02(int n)
+        {
+            //2. optimalizace
+            //Ještě více zredukujeme počet dělitelů
+            //Budeme zkoušet dělitele od 2 do odmocniny z n, pokud nenajdeme dělitele jedná se o prvočíslo
+            //Zdůvodnění:
+            //Pokud má číslo n VLASTNÍHO dělitele většího než je jeho odmocnina, pak k němu existuje i dělitel menší než odmocnina tak,
+            //že součin těchto dvou dělitelů je číslo n.
+            //Ve speciálních případech, kdy číslo n je druhá mocnina nějakého přirozeného čísla je odmocnina největší vlastní dělitel. 
+
+
+            bool prvocislo = true;
+           
+            if (n == 1) prvocislo = false;
+            for (int delitel = 2; delitel <= Math.Sqrt(n); ++delitel)
+            {
+                if (n % delitel == 0) prvocislo = false;
+            }
+            return prvocislo;
+        }
+        private bool JePrvocislo03(int n)
+        {
+            //3. optimalizace
+            //Použijeme logickou proměnnou pro zkrácení běhu programu
+            //Pokud zjistíme, že číslo n není prvočíslo (najdeme dělitele)
+            //cyklus skončí
+            //Tento  algoritmus se nezkrátí pokud n je prvočíslo
+
+            bool prvocislo = true;
+            if (n == 1) prvocislo = false;
+            int delitel = 2;
+            while (delitel <= Math.Sqrt(n) && prvocislo)
+            {
+                if (n % delitel == 0) prvocislo = false;
+                ++delitel;
+            }
+            return prvocislo;
+        }
+        private bool JePrvocislo04(int n)
+        {
+            //4. optimalizace
+            //Zkoušíme jen lichhé dělitele - sudá čísla nejsou prvočísla s výjimkou č. 2
+            //Dvojku a sudá čísla ošetříme zvlášť
+
+            bool prvocislo = true;
+
+            if (n == 1 ) prvocislo = false;
+            if (n % 2 == 0 && n != 2) prvocislo = false;
+           
+            for (int delitel = 3; delitel <= Math.Sqrt(n) && prvocislo; delitel += 2)
+            {
+                if (n % delitel == 0) prvocislo = false;
+            }
+            return prvocislo;
+        }
+
+            private void button1_Click(object sender, EventArgs e)
+        {
+            //DEFINICE: 
+            //Prvočíslo je přirozené číslo >1 dělitelné pouze 1 a sebou sama a není dělitelné žádným dalším přirozeným číslem
+            //Nejmenší prvočíslo je 2 a je to jediné sudé prvočíslo
+            //JePrvocislo00
+            int cislo = int.Parse(textBox1.Text);
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ////JePrvocislo01
+            int cislo = int.Parse(textBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //JePrvocislo02
+            int cislo = int.Parse(textBox1.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //JePrvocislo03
+            int cislo = int.Parse(textBox1.Text);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //JePrvocislo04
+            int cislo = int.Parse(textBox1.Text);
+            
+        }
+    }
+}
